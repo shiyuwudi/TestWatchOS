eace51d94d995bd720fcd4b95232db5426c1b887
shiyuwudi <289052669@qq.com>
2016-01-07 23:18:36 +0800

    初步实现前后五天的天气展示
    总的控制器是PageViewController，能做出类似书本翻页的效果。下属五个NavigationController(为了方便地显示标题
    )，以及他们的rootViewController(也就是WeatherViewController)。

--- MyWatch/ViewController.swift
+++ MyWatch/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIPageViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        dataSource = self
+        let navi = weatherViewControllerForDay(.Today)
+        setViewControllers([navi], direction: .Forward, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,10 +23,71 @@ class ViewController: UIPageViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /*
+    我们希望能为每一天的天气显示一个 title，一个比较理想的做法就是直接将我们的 WeatherViewController 嵌套在 navigation controller 里，这样我们就可以直接使用 navigation bar 来显示标题，而不用去操心它的布局了。
+    */
+    func weatherViewControllerForDay(day:WeatherViewController.Day)->UIViewController{
+        let weatherVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("WeatherViewController") as! WeatherViewController
+        let navi = UINavigationController.init(rootViewController: weatherVC)
+        weatherVC.day = day
+        return navi
+    }
+    
+}
 
+extension ViewController: UIPageViewControllerDataSource{
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        //取出日子
+        guard let navi = viewController as? UINavigationController,
+            root = navi.viewControllers.first as? WeatherViewController,
+            day = root.day
+        else{
+            return nil
+        }
+        
+        if day == .DayAfterTomorrow{
+            return nil
+        }
+        
+        //返回后一天
+        guard let laterDay = WeatherViewController.Day.init(rawValue: day.rawValue + 1) else{
+            return nil
+        }
+        
+        return weatherViewControllerForDay(laterDay)
+        
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        guard let navi = viewController as? UINavigationController,
+            root = navi.viewControllers.first as? WeatherViewController,
+            day = root.day
+            else{
+                return nil
+        }
+        
+        if day == .DayBeforeYesterday{
+            return nil
+        }
+
+        guard let earlierDay = WeatherViewController.Day.init(rawValue: day.rawValue - 1)
+            else{
+                return nil
+        }
+        
+        return weatherViewControllerForDay(earlierDay)
+        
+    }
 }
 
 class WeatherViewController: UIViewController {
+    
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var highLabel: UILabel!
+    @IBOutlet weak var lowLabel: UILabel!
     
     enum Day: Int {
         case DayBeforeYesterday = -2
@@ -31,7 +95,37 @@ class WeatherViewController: UIViewController {
         case Today
         case Tomorrow
         case DayAfterTomorrow
+        
+        //翻译
+        var title: String {
+            let translation: String
+            switch self{
+            case .DayBeforeYesterday: translation = "前天"
+            case .Yesterday: translation = "昨天"
+            case .Today: translation = "今天"
+            case .Tomorrow: translation = "明天"
+            case .DayAfterTomorrow: translation = "后天"
+            }
+            return translation
+        }
     }
     
-    var day: Day?
+    
+    var day: Day?{
+        didSet{
+            //根据成员day下title属性的变化来自动更新自己的标题
+            title = day?.title
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
